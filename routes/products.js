fdddcdf990a3f09624ae6af5a9e6ec4f2069cbc9
William Wang <haoyang324@gmail.com>
2020-03-09 17:21:36 -0700

    Update routes

---
+++ routes/products.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const auth = require('../middleware/auth')
+const productController = require('../controllers/productController')
+
+const productRouter = express.Router()
+
+productRouter.get('/', productController.getAllProduct)
+productRouter.post('/', auth.admin, productController.addProduct)
+productRouter.put('/:id', auth.admin, productController.updateProduct)
+productRouter.delete('/:id', auth.admin, productController.deleteProduct)
+
+module.exports = productRouter
